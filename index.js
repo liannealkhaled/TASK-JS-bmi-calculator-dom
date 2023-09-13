7b5d9846c9091f73c0104da8402073cdf7800299
liannealkhaled <liannealq@hotmail.com>
2023-09-13 10:51:28 +0300

    day2  task

--- index.js
+++ index.js
@@ -22,12 +22,36 @@ function calculateBMI() {
   } else {
     alert(`obesity`);
   }
+
   alert(BMI && age);
   if (BMI >= 19 && BMI <= 24 && age >= 19 && age <= 24) {
     alert(`healthy`);
   } else {
     alert(`unhealthy`);
   }
-
-  //
+  if (BMI >= 20 && BMI <= 25 && age >= 25 && age <= 34) {
+    alert(`healthy`);
+  } else {
+    alert(`unhealthy`);
+  }
+  if (BMI >= 21 && BMI <= 26 && age >= 34 && age <= 44) {
+    alert(`healthy`);
+  } else {
+    alert(`unhealthy`);
+  }
+  if (BMI >= 22 && BMI <= 27 && age >= 45 && age <= 54) {
+    alert(`healthy`);
+  } else {
+    alert(`unhealthy`);
+  }
+  if (BMI >= 23 && BMI <= 28 && age >= 55 && age <= 64) {
+    alert(`healthy`);
+  } else {
+    alert(`unhealthy`);
+  }
+  if (BMI >= 24 && BMI <= 29 && age >= 65) {
+    alert(`healthy`);
+  } else {
+    alert(`unhealthy`);
+  }
 }
